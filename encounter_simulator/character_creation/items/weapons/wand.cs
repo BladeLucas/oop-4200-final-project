23e541f1880022749647e72d06de40b1d65a4305
Blade Lucas <elffe2@hotmail.ca>
2023-02-06 09:46:11 -0500

    Added basic Wand constructor

--- encounter_simulator/character_creation/items/weapons/wand.cs
+++ encounter_simulator/character_creation/items/weapons/wand.cs
@@ -1,6 +1,22 @@
 ﻿namespace DefaultNamespace;
+using static DefaultNamespace.AllMethods;
 
 public class wand
 {
-    
+    //Variable declarations
+    public int weapon_id;
+    public string weapon_name;
+    public string attack_1;
+
+    //Creates a Wand with the parameters passed to the constructor
+    public Wand(int weapon_id, string weapon_name, string player_class, string attack_1) {
+        if(player_class != "wizard") {
+            Console.WriteLine("Only a wizard can use a wand");
+        } else {
+            weapon_id = 6;
+            weapon_name = "Wand"
+            //Setting attack to null as a wizard will use spells for attacks rather than a weapon
+            attack_1 = null;
+        }
+    }
 }
