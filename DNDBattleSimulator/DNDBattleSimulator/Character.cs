eae3cfd9904913aecfeaccfd5ba0f1a014b47e55
Matthew Taormina <matthew.taormina@dcmail.ca>
2023-03-06 12:17:24 -0500

    update many

---
+++ DNDBattleSimulator/DNDBattleSimulator/Character.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNDBattleSimulator
+{
+    class Character
+    {
+        private bool npc;
+        private string name;
+
+        // Character is an NPC property
+        public bool NPC
+        {
+            get
+            {
+                return npc;
+            }
+            set
+            {
+                npc = value;
+            }
+        }
+
+        // Read-only name property
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        // Constructor
+        public Character(string name, bool npc)
+        {
+            this.name = name;
+            this.npc = npc;
+        }
+    }
+}
